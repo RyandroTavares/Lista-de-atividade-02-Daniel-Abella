70edf902170647e97447ca4184495c903c15e9e9
RyandroTavares <ryandrotavaresbr@gmail.com>
2022-04-09 22:06:38 -0300

    Questão-03

---
+++ ex003 - Daniel Abella.py
@@ -0,0 +1,8 @@
+# 03 Faça um Programa que peça dois números e imprima a soma.
+
+numero1 = int(input('Digite um número: '))
+numero2 = int(input('Digite outro número: '))
+soma = numero1 + numero2
+print()
+
+print('A soma dos número é {}'.format(soma))
